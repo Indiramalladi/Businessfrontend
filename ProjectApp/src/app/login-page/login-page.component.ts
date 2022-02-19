f8853c2c9e5213bb75ef8ea54b26ec54d95e7cdd
malladi indira <malladi.indira1999@gmail.com>
2022-02-19 21:34:27 +0530

    myfile

---
+++ ProjectApp/src/app/login-page/login-page.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
+})
+export class LoginPageComponent {
+  user:any={};
+}
